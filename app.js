eb9fe825de9f8e3d5c203dd474d85d162ec47247
FrederikALP <fred05k3@stud.kea.dk>
2022-02-21 00:34:39 +0100

    fixed heroku things?

--- app.js
+++ app.js
@@ -2,22 +2,13 @@ const express = require("express");
 const app = express();
 app.use(express.static('public'));
 
-// "" does not allow multiple lines
-// `` allows multiple lines
-app.get("/", (req, res) => {
-    res.send(`
-    <h1>Welcome to my website</h1>
-    <h2>Take a look ${true}</h2>
-    `);
-});
-
 // sendFile to send a file as a response
-app.get("/clock", (req, res) => {
+app.get("/", (req, res) => {
     // __dirname gives the path of where the file it is written is located
     res.sendFile(__dirname + "/public/clock.html");
 });
 
 
-app.listen(8080, () => {
-    console.log("The Server is running", 8080);
+app.listen(process.env.PORT || "5000", () => {
+    console.log("The Server is running", 5000);
 });
